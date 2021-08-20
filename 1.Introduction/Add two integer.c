dd70d8a21d36615e8b724f3c8df4c5cc052a0e7f
Aripandi <89141902+aripandi01@users.noreply.github.com>
2021-08-20 18:14:41 +0530

    Add files via upload

---
+++ 1.Introduction/Add two integer.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+    int a,b,c;
+
+    //get the a and b value from user and store it
+    printf("Enter the A value: ");
+    scanf("%d",&a);
+    printf("Enter the B value: ");
+    scanf("%d",&b);
+
+    c=a+b;
+
+    //print the c value
+    printf("Added value C : %d",c);
+}
