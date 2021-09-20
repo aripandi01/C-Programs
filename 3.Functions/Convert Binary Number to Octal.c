b3811197fd0aa660cdb0016bd31ca07868ede8ab
Aripandi <89141902+aripandi01@users.noreply.github.com>
2021-09-20 08:37:35 +0530

    Add files via upload

---
+++ 3.Functions/Convert Binary Number to Octal.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<math.h>
+
+int convert(long long bin);
+int main()
+{
+    long long bin;
+    printf("Enter the Binary Number: ");
+    scanf("%lld",&bin);
+    printf("%lld in binary = %d in octal", bin, convert(bin));
+    return 0;
+}
+
+int convert(long long bin)
+{
+   int oct = 0,dec = 0,i=0;
+
+   //Binary to decimal
+   while(bin!=0)
+   {
+       dec += (bin % 10) * pow(2,i);
+       i++;
+       bin /= 10;
+   }
+   i=1;
+
+   //Decimal to Octal
+   while(dec!=0)
+   {
+       oct += (dec %8)*i;
+       dec /=8;
+       i *= 10;
+   }
+
+   return oct;
+}
